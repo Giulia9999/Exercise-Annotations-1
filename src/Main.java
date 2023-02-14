50e88a130b8e9af258c02767a6c362aaeaefbb6e
Giulia <giuliac35@gmail.com>
2023-02-14 20:37:17 +0100

    Create main method and instance of Car, call thr 2 methods of Car class. Add suppressWarnings annotation

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,9 @@
+public class Main {
+    @SuppressWarnings(value = "deprecation")
 
+    public static void main(String[] args) {
+        Car car = new Car("A4", 200000);
+        car.printCarDetails();
+        car.getCarDetails();
+    }
+}
