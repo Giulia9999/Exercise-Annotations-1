02bab44b796e5317db80269344927c7cebe69f66
Giulia <giuliac35@gmail.com>
2023-02-14 16:14:00 +0100

    Create constructor of Car class

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,9 @@
 public class Car {
+    private String modelName;
+    private int mileage;
 
+    public Car(String modelName, int mileage){
+        this.modelName = modelName;
+        this.mileage = mileage;
+    }
 }
